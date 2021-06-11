fb414a0fe4be952e31d6f3cf7951b145ca47d01f
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2021-06-11 15:49:01 +0200

    Create geometry.py

---
+++ basic-math-package/geometry.py
@@ -0,0 +1,11 @@
+def area_of_rectangle(length, breadth):
+    """
+    This function returns the area of a rectangle
+    """
+    return length*breadth
+
+def area_of_circle(radius):
+    """
+    This function returns the area of a circle
+    """
+    return 2*3.14159265359*radius^2
